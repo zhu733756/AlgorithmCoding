57df05ef1c0ddcfd124e972d205992b7914c9be3
zhu733756 <talonzhu@yunify.com>
2021-09-28 18:22:01 +0800

    反转有序数组查找元素
    Signed-off-by: zhu733756 <talonzhu@yunify.com>

--- 二分查找/demo/search-in-rotated-sorted-array-2.py
+++ 二分查找/demo/search-in-rotated-sorted-array-2.py
@@ -1,3 +1,29 @@
 """
 https://leetcode-cn.com/problems/search-in-rotated-sorted-array-ii/
 """
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> bool:
+        '''二分查找'''
+        left,right =0,len(nums)-1
+        while left<=right:
+            mid = (left+right) //2
+            if nums[mid] == target:
+                return True
+            # 考虑到左右两边相等，如[1,0,1,1]
+            if nums[mid] == nums[left] == nums[right]:
+                left +=1
+                right -= 1
+            elif nums[mid] >= nums[left]:
+                # [left, mid] 有序
+                if nums[left] <= target < nums[mid]:
+                    right = mid -1
+                else:
+                    left = mid+1
+            elif nums[mid] <= nums[right]:
+                # [mid, right] 有序
+                if nums[mid] < target <= nums[right]:
+                    left = mid + 1
+                else:
+                    right = mid -1
+        return False
